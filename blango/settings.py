ca0b1cc922ba06bdbf525abf3d960d6987480bb2
MOAD IBOUREK <mouad.ibourek@emsi-edu.ma>
2024-05-29 19:29:59 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -197,6 +197,11 @@ class Dev(Configuration):
 
   INTERNAL_IPS = ["192.168.11.179"]
 
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+  ACCOUNT_ACTIVATION_DAYS = 7
+
+
 
 
 class Prod(Dev):
